5152aa8007b15da1be2ff06332d3100c96396a0b
vjtsieuthit96 <vonguyenhung96@gmail.com>
2025-08-10 04:59:29 +0700

    Update Player Corgi

--- Assets/Scripts/PlayerSpawner.cs
+++ Assets/Scripts/PlayerSpawner.cs
@@ -8,5 +8,9 @@ public class PlayerSpawner : SimulationBehaviour, IPlayerJoined
     public void PlayerJoined(PlayerRef player)
     {
         print($"Player {player.PlayerId} joined the game. Spawning player object...");
+        if(player == Runner.LocalPlayer)
+        {
+            Runner.Spawn(_playerPrefab, new Vector3(0, 1, 0), Quaternion.identity, player);
+        }       
     }
 }
